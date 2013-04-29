64f8455c8efaae53778943507791873df3f8cacc
Adam Borocz <boroczadam@gmail.com>
2013-04-29 20:59:47 +0100

    Update recipe list in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 supports 'ubuntu', ">= 12.04"
+
+recipe "hipsnip-mongodb", "Downloads and unpacks the required version of MongoDB - does not set up an instance"
+recipe "hipsnip-mongodb::mongo_gem", "Installs the required version of the mongo and bson_ext gems"
+recipe "hipsnip-mongodb::mongod", "Sets up a single mongod instance - don't use for replica sets"
+recipe "hipsnip-mongodb::replica_set", "Automatically sets up and configures a replica set using Chef Search"
