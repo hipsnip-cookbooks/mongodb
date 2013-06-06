a364495cdecc9c5dfd18e35f5019f3bd2a9e1cef
Adam Borocz <boroczadam@gmail.com>
2013-06-06 15:24:36 +0100

    Bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email "adam@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.1.0"
 supports 'ubuntu', ">= 12.04"
+
+depends "sysctl", "~> 0.3.2"
 
 recipe "hipsnip-mongodb", "Downloads and unpacks the required version of MongoDB - does not set up an instance"
 recipe "hipsnip-mongodb::mongo_gem", "Installs the required version of the mongo and bson_ext gems"
