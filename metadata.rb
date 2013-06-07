c180a6cf9d517473d2c73374567cff151fa73a02
Adam Borocz <boroczadam@gmail.com>
2013-06-07 14:20:18 +0100

    Update cookbook dependency for sysctl

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
 supports 'ubuntu', ">= 12.04"
 
-depends "sysctl", "~> 0.3.2"
+depends "sysctl", ">= 0.3.2"
 
 recipe "hipsnip-mongodb", "Downloads and unpacks the required version of MongoDB - does not set up an instance"
 recipe "hipsnip-mongodb::mongo_gem", "Installs the required version of the mongo and bson_ext gems"
