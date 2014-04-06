f0ce9cab930e7e49bbd05fb19719e886f2a0ed9f
Adam Borocz <boroczadam@gmail.com>
2014-04-06 23:32:08 +0100

    Make user provider more resilient
    Setting permissions for a replica set on the initial chef run may
    sometimes fail, probably due to the replica set still being in an
    inconsistent (initial) state - despite the cluster reporting that it’s
    ready. Utilising the same retry mechanism we have for node and replica
    set state checks seems to solve the problem.

--- providers/user.rb
+++ providers/user.rb
@@ -28,45 +28,60 @@ action :create do
   admin_pass = node['mongodb']['admin_user']['password']
   auth_enabled = node['mongodb']['auth_enabled']
 
-  Chef::Log.info "Setting up User #{username} for database #{database} on MongoDB node at #{node_ip}:#{port}"
+  Chef::Log.info "Setting up User '#{username}' for database '#{database}' on MongoDB node at #{node_ip}:#{port}"
 
   unless auth_enabled
     raise 'You must have auth enabled before you can create users'
   end
 
-  # Try and work out if we're a replica set
-  begin
-    Chef::Log.info "Trying to connect to replica set for User creation"
-    connection = ::Mongo::MongoReplicaSetClient.new(["#{node_ip}:#{port}"], :read => :primary_preferred, :connect_timeout => 10)
-  rescue ::Mongo::ConnectionFailure => ex
-    raise unless ex.message.include? 'Cannot connect to a replica set'
-    Chef::Log.info "Trying to connect to single node for User creation"
-    # This probably means we're just a single-node instance
-    connection = ::Mongo::MongoClient.new(node_ip, port, :slave_ok => true)
-  end
-
-  db = connection['admin']
+  retries = 0
 
   begin
-    db.authenticate(admin_user, admin_pass)
-  rescue ::Mongo::AuthenticationError
-    # Fail silently
-    # This could be a genuine failure, or it could just be that
-    # the credential setup provider hasn't run yet. If this really
-    # is a failure, the serverStatus command below will catch it.
-    Chef::Log.warn("Authentication as admin to MongoDB node at #{node_ip}:#{port} failed")
-  end
+    # Try and work out if we're a replica set
+    begin
+      Chef::Log.info "Trying to connect to replica set for User creation"
+      connection = ::Mongo::MongoReplicaSetClient.new(["#{node_ip}:#{port}"], :read => :primary_preferred, :connect_timeout => 10)
+    rescue ::Mongo::ConnectionFailure => ex
+      raise unless ex.message.include? 'Cannot connect to a replica set'
+      Chef::Log.info "Trying to connect to single node for User creation"
+      # This probably means we're just a single-node instance
+      connection = ::Mongo::MongoClient.new(node_ip, port, :slave_ok => true)
+    end
 
-  db = connection[database]
+    db = connection['admin']
 
-  begin
+    begin
+      db.authenticate(admin_user, admin_pass)
+    rescue ::Mongo::AuthenticationError
+      # Fail silently
+      # This could be a genuine failure, or it could just be that
+      # the credential setup provider hasn't run yet. If this really
+      # is a failure, the serverStatus command below will catch it.
+      Chef::Log.warn("Authentication as admin to MongoDB node at #{node_ip}:#{port} failed")
+    end
+
+    db = connection[database]
+
     # It appears that the Ruby driver does all the work for us here:
     # 1. If the user doesn't exist -> create it
     # 2. If the user exists -> update password and roles accordingly
     db.add_user(username, password, false, "roles" => roles)
   rescue ::Mongo::ConnectionFailure => ex
     # Expected error on first run of script
-    raise unless ex.message.include? 'not master or secondary; cannot currently read from this replSet member'
+    unless ex.message.include? 'not master or secondary; cannot currently read from this replSet member'
+      raise if retries >= node['mongodb']['node_check']['retries']
+      Chef::Log.warn "Failed to set permissions for user '#{username}' on database '#{database}'"
+
+      begin
+        connection.close
+      rescue
+      end
+
+      retries += 1
+      sleep_time = retries * node['mongodb']['node_check']['timeout']
+      Chef::Log.info "Waiting #{sleep_time} seconds and retrying..."
+      sleep(sleep_time)
+    end
   end
 
   Chef::Log.info "User #{username} for database #{database} created/updated on MongoDB node at #{node_ip}:#{port}"
